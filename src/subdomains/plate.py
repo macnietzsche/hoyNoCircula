9e3c1d849318121ea5d774d269b08a7a350cb31a
mvillegas <mbvillegas94@gmail.com>
2021-08-05 16:20:02 -0500

    REFACTOR: License plates subdomain: TDD Green

--- src/subdomains/plate.py
+++ src/subdomains/plate.py
@@ -1,12 +1,14 @@
+import re
 class LicensePlate:
     def __init__(self,plate) -> None:
         self.plate=plate
 
     @property
     def plate(self):
-        return self.plate
+        return self._plate
 
     @plate.setter
     def plate(self,plate):
-        return None
-        # raise Exception()
+        matched_plate_pattern = re.match(r'^[A-Z]{3}-\d{3,4}$',plate)
+        if not bool(matched_plate_pattern): raise Exception()
+        self._plate=plate
--- test/plates/test_plates.py
+++ test/plates/test_plates.py
@@ -3,14 +3,16 @@ from src.subdomains.plate import LicensePlate
 
 class TestPlatesSubdomain(TestCase):
     def test_plate_data_type_is_incorrect(self):
-        incorrect_plate="T*V-12345"
+        incorrect_plates=["T*V-12345","78J-123F","*-+-12345,AAAA-123456","aaa1234","AAA1234","1231234","AAA-34**"]
         with self.assertRaises(Exception):
-            LicensePlate(incorrect_plate)
+            for incorrect_plate in incorrect_plates:
+                LicensePlate(incorrect_plate)
 
     def test_plate_data_type_is_correct(self):
-        correct_plate="TBB-12345"
-        try:
-            LicensePlate(correct_plate)
-        except Exception:
-            assert False
+        correct_plates=["TBF-1234","PTT-123","AAA-0826","AAA-4666"]
+        for correct_plate in correct_plates:
+            try:
+                LicensePlate(correct_plate)
+            except Exception:
+                assert False
     
