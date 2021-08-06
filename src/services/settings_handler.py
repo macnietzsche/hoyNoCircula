ceb54ca3e6b820b053eb7da232323161ba7c1ad4
mvillegas <mbvillegas94@gmail.com>
2021-08-06 18:38:49 -0500

    REFACTOR: Settings handler. TDD Green.

--- src/services/settings_handler.py
+++ src/services/settings_handler.py
@@ -1,3 +1,12 @@
 class SettingsHandler():
     def __init__(self,day_of_week_as_integer) -> None:
         self.day_of_week_as_integer=day_of_week_as_integer
+
+    @property
+    def day_of_week_as_integer(self):
+        return self._day_of_week_as_integer
+    
+    @day_of_week_as_integer.setter
+    def day_of_week_as_integer(self,day_of_week_as_integer):
+        if not (isinstance(day_of_week_as_integer, int) and day_of_week_as_integer>=0 and day_of_week_as_integer<=6): raise Exception("Day parameter is not valid")
+        self._day_of_week_as_integer=day_of_week_as_integer
