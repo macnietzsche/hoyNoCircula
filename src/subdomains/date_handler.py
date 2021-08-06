6600bd257fbb1ab2fe9fb80530ad667a11a5dd90
mvillegas <mbvillegas94@gmail.com>
2021-08-05 19:55:51 -0500

    REFACTOR: Deleted unnecessary setter

--- src/subdomains/date_handler.py
+++ src/subdomains/date_handler.py
@@ -1,15 +1,7 @@
 from datetime import datetime
 class DateHandler:
     def __init__(self,date_time_input) -> None:
-        self.date_time_object=date_time_input
-
-    @property
-    def date_time_object(self):
-        return self._date_time_object
-
-    @date_time_object.setter
-    def date_time_object(self,date_time_input):
-        self._date_time_object=datetime.strptime(date_time_input,"%Y-%m-%d %H:%M")
+        self.date_time_object=datetime.strptime(date_time_input,"%Y-%m-%d %H:%M")
     
         
 
