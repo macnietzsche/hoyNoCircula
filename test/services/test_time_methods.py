94e445d7c420ed74e4ad40b58be7c6cf42aadda0
mvillegas <mbvillegas94@gmail.com>
2021-08-05 22:24:41 -0500

    REFACTOR: Time methods. TDD Green

--- test/services/test_time_methods.py
+++ test/services/test_time_methods.py
@@ -9,10 +9,39 @@ class TestTimeMethods(TestCase):
         self.assertTrue(is_time_in_range(*times))
 
         #Array elements go in this order: start time, end time, trial time
-        times=[time(14,23),time(14,23),time(15,00)]
+        times=[time(14,23),time(14,23),time(14,23)]
         self.assertTrue(is_time_in_range(*times))
 
-        #Array elements go in this order: start time, end time, trial time
+        # #Array elements go in this order: start time, end time, trial time
         times=[time(0,0),time(1,30),time(0,30)]
         self.assertTrue(is_time_in_range(*times))
+
+        # #Array elements go in this order: start time, end time, trial time
+        times=[time(0,0),time(0,0),time(0,0)]
+        self.assertTrue(is_time_in_range(*times))
+
+        # #Array elements go in this order: start time, end time, trial time
+        times=[time(8,35),time(10,0),time(8,35)]
+        self.assertTrue(is_time_in_range(*times))
+
+         # #Array elements go in this order: start time, end time, trial time
+        times=[time(8,35),time(12,49),time(12,49)]
+        self.assertTrue(is_time_in_range(*times))
+
+    def test_is_not_time_in_range(self):
+        #Array elements go in this order: start time, end time, trial time
+        times=[time(7,0),time(10,30),time(10,31)]
+        self.assertFalse(is_time_in_range(*times))
+
+        #Array elements go in this order: start time, end time, trial time
+        times=[time(14,23),time(14,23),time(14,24)]
+        self.assertFalse(is_time_in_range(*times))
+
+        #Array elements go in this order: start time, end time, trial time
+        times=[time(0,0),time(1,30),time(2,30)]
+        self.assertFalse(is_time_in_range(*times))
+
+        #Array elements go in this order: start time, end time, trial time
+        times=[time(0,0),time(0,0),time(0,1)]
+        self.assertFalse(is_time_in_range(*times))
 
