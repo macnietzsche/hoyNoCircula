c392a40b88abe551449839d7205db7d4657399ba
mvillegas <mbvillegas94@gmail.com>
2021-08-05 23:35:04 -0500

    REFACTOR: Date handler. TDD red

--- src/subdomains/date_handler.py
+++ src/subdomains/date_handler.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 class DateHandler:
     def __init__(self,date_time_input) -> None:
-        self.date_time_object=datetime.strptime(date_time_input,"%Y-%m-%d %H:%M")
+        self.date_time_object=date_time_input
+    @property
+    def date_time_object(self):
+        return self._date_time_object
+
+    @date_time_object.setter
+    def date_time_object(self,date_time_input):
+        try:
+            self._date_time_object=datetime.strptime(date_time_input,"%Y-%m-%d %H:%M")
+        except ValueError:
+            raise Exception("Datetime input is not valid")
 
     def get_day_as_integer(self):
         return self.date_time_object.weekday()
--- test/date_handler/test_date_handler.py
+++ test/date_handler/test_date_handler.py
@@ -21,6 +21,18 @@ class TestDateTime(TestCase):
 
         with self.assertRaises(Exception):
             DateHandler("2012-09-31 00:00")
+        
+        with self.assertRaises(Exception):
+            DateHandler("2012-09-31 99:00")
+        
+        with self.assertRaises(Exception):
+            DateHandler("2012-09-31 34:99")
+        
+        with self.assertRaises(Exception):
+            DateHandler("34:99")
+
+        with self.assertRaises(Exception):
+            DateHandler("14:99")
 
     def test_datetime_input_is_correct(self):
         try:
@@ -37,24 +49,40 @@ class TestDateTime(TestCase):
             DateHandler("2024-02-28 23:59")
         except Exception:
             assert False
+        
+        try:
+            DateHandler("23:59")
+        except Exception:
+            assert False
+
+        try:
+            DateHandler("22:19")
+        except Exception:
+            assert False
 
     def test_get_day_of_the_week(self):
         date1=DateHandler("2020-12-01 20:23")
         self.assertEqual(date1.get_day_as_integer(),1)
 
-        date1=DateHandler("2021-10-09 20:23")
-        self.assertEqual(date1.get_day_as_integer(),5)
+        date2=DateHandler("2021-10-09 20:23")
+        self.assertEqual(date2.get_day_as_integer(),5)
 
-        date1=DateHandler("2024-02-28 20:23")
-        self.assertEqual(date1.get_day_as_integer(),2)
+        date3=DateHandler("2024-02-28 20:23")
+        self.assertEqual(date3.get_day_as_integer(),2)
+
+        date4=DateHandler("20:23")
+        self.assertEqual(date4.get_day_as_integer(),-1)
+
+        date5=DateHandler("00:23")
+        self.assertEqual(date5.get_day_as_integer(),-1)
 
     def test_get_hour_as_time_object(self):
         date1=DateHandler("2020-12-01 20:23")
         self.assertEqual(date1.get_hour_as_time_object(),time(20,23))
 
-        date1=DateHandler("2021-10-09 00:00")
-        self.assertEqual(date1.get_hour_as_time_object(),time(0,0))
+        date2=DateHandler("2021-10-09 00:00")
+        self.assertEqual(date2.get_hour_as_time_object(),time(0,0))
 
-        date1=DateHandler("2024-02-28 09:35")
-        self.assertEqual(date1.get_hour_as_time_object(),time(9,35))
+        date3=DateHandler("2024-02-28 09:35")
+        self.assertEqual(date3.get_hour_as_time_object(),time(9,35))
 
