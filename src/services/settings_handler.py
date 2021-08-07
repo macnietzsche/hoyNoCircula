fdd30b05139a99046a97e52a5b5947b4d1629b87
mvillegas <mbvillegas94@gmail.com>
2021-08-06 19:30:38 -0500

    REFACTOR: Retricted times and plates. TDD Green.

--- src/services/settings_handler.py
+++ src/services/settings_handler.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from src.subdomains.date_handler import DateHandler
 
 day_names_hashmap={0: "MONDAY",1:"TUESDAY",2:"WEDNESDAY",3:"THURSDAY",4:"FRIDAY",5:"SATURDAY",6:"SUNDAY"}
 class SettingsHandler():
@@ -18,7 +19,19 @@ class SettingsHandler():
         
 
     def get_restricted_time_segments(self):
-        return None
+        time_segments = self._settings_of_day['restricted_time'].split("/")
+        time_segments_list=[]
+        for time_segment in time_segments:
+            (start_time,end_time)=time_segment.split("-")
+            start_time_as_date=DateHandler(start_time).get_hour_as_time_object()
+            end_time_as_date=DateHandler(end_time).get_hour_as_time_object()
+            if(end_time_as_date>=start_time_as_date):
+                time_segments_list.append((start_time_as_date,end_time_as_date))
+            else:
+                raise Exception("End time must be greater or equals than start time")
+       
+        return time_segments_list
     
     def get_restricted_plate_ending_digits(self):
-        return None
+         digits_as_characters=self._settings_of_day['restricted_plate_ending_digit'].split(",")
+         return list(map(lambda x: int(x),digits_as_characters))
