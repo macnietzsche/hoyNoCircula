c60fa40713adb596bf6d1e62113599a3b42378d6
mvillegas <mbvillegas94@gmail.com>
2021-08-06 22:30:38 -0500

    ADD: App initializer

--- src/app.py
+++ src/app.py
@@ -1,2 +1,16 @@
+import json
+from predictor.predictor import Predictor
+
 if __name__ == '__main__':
-    print("Hoy no circula - Predictor")
+    print("Pico y Placa - Predictor")
+    with open('plates_input.json') as file:
+        data = json.load(file)
+    
+    for entry in data:
+        plate=entry['plate']
+        date_n_time=entry['datetime']
+        prediction=Predictor(date_n_time,plate)
+        if prediction.can_my_vehicle_be_on_the_road():
+            print(f'Vehicle with plates {plate} can be on the road on {date_n_time}')
+        else:
+            print(f'Vehicle with plates {plate} cannot be on the road on {date_n_time}')    
