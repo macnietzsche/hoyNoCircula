5c80f92bff93b7d0c2dfee4173d501f57dc548a7
mvillegas <mbvillegas94@gmail.com>
2021-08-06 20:31:07 -0500

    REFACTOR: Predictor function. TDD Green

--- src/predictor/predictor.py
+++ src/predictor/predictor.py
@@ -8,7 +8,6 @@ class Predictor:
         self.date_n_time=DateHandler(date_n_time)
         self.plates=LicensePlate(plates)
 
-    
     def can_my_vehicle_be_on_the_road(self)->bool:
         day_of_the_week_as_integer=self.date_n_time.get_day_as_integer()
         time=self.date_n_time.get_hour_as_time_object()
@@ -18,14 +17,10 @@ class Predictor:
         restricted_time_segments=day_settings.get_restricted_time_segments()
         restricted_plate_last_digits=day_settings.get_restricted_plate_ending_digits()
 
-        
+        if not last_digit_of_plate in restricted_plate_last_digits: return True
         for restricted_time_segment in restricted_time_segments:
-            if is_time_in_range(*restricted_time_segment,time): 
-                if last_digit_of_plate in restricted_plate_last_digits:
-                     return False
-                else: 
-                    break
-
+            if is_time_in_range(*restricted_time_segment,time): return False
+               
         return True
     
 
